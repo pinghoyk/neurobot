8f1ed768e24e65ff1bcfbc2d854e4ed6d9bd607c
pinghoyk <pinghoyk009@gmail.com>
2025-11-25 17:22:06 +0500

    Загружаем переменные и проверяем их на наличие

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,3 +15,33 @@ type Config struct {
 	DatabasePath     string
 }
 
+// Загружаем конфиг и ищем файл .env
+func Load() (*Config, error) {
+	_ = godotenv.Load()
+
+	cfg := &Config{
+		TelegramBotToken: os.Getenv("TG_BOT_TOKEN"),
+		GigaChatClientID: os.Getenv("GIGACHAT_CLIENT_ID"),
+		GigaChatSecret:   os.Getenv("GIGACHAT_SECRET"),
+		GigaChatScope:    os.Getenv("GIGACHAT_SCOPE"),
+		DatabasePath:     getEnvOrDefault("DATABASE_PATH", "bot.db"),
+	}
+
+	if cfg.TelegramBotToken == "" {
+		return nil, fmt.Errorf("Требуется - TG_BOT_TOKEN")
+	}
+
+	if cfg.GigaChatClientID == "" {
+		return nil, fmt.Errorf("Требуется - GIGACHAT_CLIENT_ID")
+	}
+
+	if cfg.GigaChatSecret == "" {
+		return nil, fmt.Errorf("Требуется - GIGACHAT_SECRET")
+	}
+
+	if cfg.GigaChatScope == "" {
+		cfg.GigaChatScope = "GIGACHAT_API_CORP"
+	}
+
+	return cfg, nil
+}
