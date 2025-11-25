e627d0dfe776d114a92a5f47295df4dc4fa3ea51
pinghoyk <pinghoyk009@gmail.com>
2025-11-25 17:09:23 +0500

    Структура настроек предпочтений

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -8,3 +8,13 @@ type UserState struct {
 	InputData     string   // временные данные от пользователя (напр., введённые ингредиенты)
 	StateHistory  []string // стек состояний - чтобы можно было сделать "назад"
 }
+
+// UserPreferences представляет кулинарные предпочтения пользователя
+type UserPreferences struct {
+	UserID       int64
+	DietaryType  string // обычное, похудение, набор веса
+	Goal         string // цель: (напр., похудеть на 3 кг)
+	Allergies    string // данные, на что аллергия
+	Likes        string // данные, что нравится в еде
+	Dislikes     string // данные, что не нравится в еде
+}
