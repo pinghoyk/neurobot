3ca9c2a4010f2d9d82e00ae32f6ef51287ed6d93
pinghoyk <pinghoyk009@gmail.com>
2025-11-25 14:23:13 +0500

    Создаем базу данных

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -20,4 +20,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Не удалось загрузить конфигурацию: %v", err)
 	}
+
+	// Создание базы данных
+	log.Println("Создание базы данных...")
+	db, err := database.New(cfg.DatabasePath)
+	if err != nil {
+		log.Fatalf("Не удалось создать базу данных: %v", err)
+	}
+	defer db.Close() // Закрыть соединение с БД при завершенииы
 }
